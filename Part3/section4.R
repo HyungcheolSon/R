b356759e71bf8d0460eb142a1634adee202aa3e2
HyungcheolSon <51064999+HyungcheolSon@users.noreply.github.com>
2019-05-29 20:46:27 +0900

    Update section4.R

--- Part3/section4.R
+++ Part3/section4.R
@@ -41,3 +41,11 @@ sum(1, NA, 2)
 setwd("D:/workspace/R_Data_Analysis/Part3/data")
 txt1 <- read.csv("factor_test.txt")
 txt1
+
+factor1 <- factor(txt1$blood)
+factor1
+
+summary(factor1)
+
+sex1 <- factor(txt1$sex)
+summary(sex1)
