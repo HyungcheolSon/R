1f314d83ee1f4997b7c1184bb2eb3e9093610f67
HyungcheolSon <applepie8817@gmail.com>
2019-05-29 17:22:01 +0900

    Part 3, section 4

---
+++ Part3/section1~3.R
@@ -0,0 +1,30 @@
+setwd("D:/workspace/R_Data_Analysis/Part3/data")
+print(3)
+
+print(1+2)
+
+print("a")
+
+print('a')
+
+print("apple")
+
+'String'
+
+print(pi)
+
+print(pi,digits=3)
+
+print(3,4)
+
+
+1;2;3
+1+2;2*3;4/2
+
+100000
+1000000000000000000
+# indentation
+# Comment 
+
+
+
---
+++ Part3/section4.R
@@ -0,0 +1,43 @@
+print(3+4)
+
+print(3%%3)
+
+print((3*2)%%3) 
+
+print(3^2)
+
+1/100000
+
+'First'
+
+first <- 1
+
+cat(1,NULL,2)
+
+cat(1, NA, 2)
+
+print('d')
+
+cat(1,NA,2)
+
+sum(1,NULL,2)
+
+date()
+
+as.numeric ('1') +as.numeric('2')
+
+print(3 & 0)
+
+class (1)
+
+class ('1')
+
+cat(1, NA, 2)
+
+cat(1, NULL, 2)
+
+sum(1, NA, 2)
+
+setwd("D:/workspace/R_Data_Analysis/Part3/data")
+txt1 <- read.csv("factor_test.txt")
+txt1
