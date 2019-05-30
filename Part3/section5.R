142c7ed7c7be7d19a8a026ec36c8e17319b7af36
HyungcheolSon <51064999+HyungcheolSon@users.noreply.github.com>
2019-05-30 12:37:32 +0900

    Add files via upload

---
+++ Part3/section5.R
@@ -0,0 +1,30 @@
+var1 <- "aaa"
+var2 <- 111
+var3 <- Sys.Date()
+var4 <- c("a","b","c")
+var1 
+var2
+var3
+var4
+111 -> var5 -> var6
+String1 <-"So Easy R Programming"
+String1
+String2 <- "I'm Hyungcheol Son"
+String2
+comp <-c(1,"2")
+comp
+class(comp)
+num1<-1
+num2<-2
+num1+num2
+seq1<-1:5
+seq1
+seq2<-1:11
+seq2
+String2
+rm(String2)
+String2
+objects()
+rm(list=ls())
+object()
+character(0)
---
+++ Part3/section6.R
@@ -0,0 +1,69 @@
+c(1,2,3,4,5)
+c(1,2,3,4,"5")
+vec1<-c(1,2,3,4,5)
+vec1
+vec1[3]
+vec1[-3]
+vec1[-1:-3]
+vec1<-c(1,2,3,4,5)
+vec1[2]
+vec1[2]<-6
+vec1
+vec1[5]<-8
+vec1
+vec1<-c(vec1,7,9)
+vec1
+vec1[9]<-9
+vec1
+vec1[8]<-4
+vec1
+vec1<-c(vec1,10,11)
+vec1
+vec1<-rm(vec1)
+vec1
+append(vec1,11,after=3)
+vec1
+vec1<-c(1,2,3,4,5,6,7,8,9,10)
+append(vec1,10,after=3)
+vec1
+vec1<-rm(list=c(1,2,3))
+vec2<-c(1,2,3,4,5)
+vec2
+
+c(1,2,3) + c(4,5,6)
+c(1,2,3) + 1
+var1 <- c(1,2,3)
+var2 <- c(3,4,5)
+var1 + var2
+var3 <- c('3','4',5)
+var1 + var3
+union(var1,var3)
+var4<-c(1,2,3,4,5)
+var1
+var4
+var1+var4
+var1
+var2
+var1-var2
+setdiff(var1,var2)
+setdiff(var2,var1)
+intersect(var1,var2)
+fruits<-c(10,20,30)
+fruits
+names(fruits)<-c('apple','banana','peach')
+fruits
+var5<-seq(1,5)
+var5
+var6<-seq(2,-2)
+var6
+var7<-seq(1,10,2) #odd
+var7
+3 %in%var7
+4 %in%var7
+var17<-seq(2,10,2) #even
+var17
+var8<-rep(1:3,2)
+var8
+var9<-rep(1:3,each=2)
+var9
+
