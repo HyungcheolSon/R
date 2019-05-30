bebe4f33bc6f372d9af1d96ada726930fb8d6ed0
HyungcheolSon <applepie8817@gmail.com>
2019-05-30 12:35:36 +0900

    Part3, section 5

--- Part3/section4.R
+++ Part3/section4.R
@@ -39,6 +39,9 @@ cat(1, NULL, 2)
 sum(1, NA, 2)
 
 setwd("D:/workspace/R_Data_Analysis/Part3/data")
+getwd()
+
+setwd("D:/workspace/R_Data_Analysis/Part3/data")
 txt1 <- read.csv("factor_test.txt")
 txt1
 
@@ -49,3 +52,46 @@ summary(factor1)
 
 sex1 <- factor(txt1$sex)
 summary(sex1)
+
+Sys.Date()
+
+Sys.time()
+
+date()
+
+as.Date('2019-5-30')
+
+class("2019-05-30")
+
+as.Date("2019-05-30")
+
+as.Date("30052019",format="%d%m%Y")
+
+as.Date("300519",format="%d%m%y")
+
+as.Date(20,origin="2019-5-30")
+
+as.Date(-20,origin="2019-5-30")
+
+class("2019-5-30")
+
+as.Date("2019-5-30")-as.Date("2019-5-01")
+
+as.Date("2019-5-30") + 5
+
+as.Date("2019-11-14") - Sys.Date()
+
+install.packages("lubridate")
+library(lubridate)
+date <- as.POSIXct("2019-5-30 9:50:11 KST")
+date
+
+year(date)
+month(date,label=T)
+day(date)
+wday(date,label=T)
+month(date)<-2
+date
+date+as.POSIXct(1)
+
+
